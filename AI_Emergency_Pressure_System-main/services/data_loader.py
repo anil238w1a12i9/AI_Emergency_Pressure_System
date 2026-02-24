6a073a8a97bef946f8d838a75d593679724989bb
T.Anil kumar <153080345+anil238w1a12i9@users.noreply.github.com>
2026-02-24 16:41:52 +0530

    Delete AI_Emergency_Pressure_System-main directory

--- AI_Emergency_Pressure_System-main/services/data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_data():
-    accidents = pd.read_csv("data/accidents.csv")
-    ambulance = pd.read_csv("data/ambulance_logs.csv")
-    weather = pd.read_csv("data/weather.csv")
-    festivals = pd.read_csv("data/festivals.csv")
-    return accidents, ambulance, weather, festivals
